0463cb92b9c4654d14d15b44cea86bd9121383df
noskla <a@horsi.fi>
2021-02-13 09:36:22 +0100

    💄 Use existing getDirectoryPath implementation

--- front.go
+++ front.go
@@ -1,11 +1,9 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
 )
 
 // FrontRouter represents the Gin router group dedicated for the front-end features.
@@ -14,14 +12,7 @@ var FrontRouter *gin.RouterGroup
 // InitFrontRouter initializes the FrontRouter variable with Gin router group,
 // loads HTML templates and parses SASS stylesheets.
 func InitFrontRouter(router *gin.Engine) {
-	templatePath := Here + "/templates/"
-	if _, err := os.Stat(templatePath); os.IsNotExist(err) {
-		// try finding templates relatively
-		templatePath = "templates/"
-		if _, err = os.Stat(templatePath); os.IsNotExist(err) {
-			log.Fatalln("Could not find template directory.")
-		}
-	}
+	templatePath := getDirectoryPath("templates/")
 
 	log.Println("Loading HTML templates at", templatePath)
 	router.LoadHTMLGlob(templatePath + "*")
