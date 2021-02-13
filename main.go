a691cccdbda18d5dc6451af7982d7590f61eaf39
noskla <a@horsi.fi>
2021-02-13 09:19:29 +0100

    🐛 Log errors coming out of HTTP server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -8,15 +9,19 @@ import (
 
 // Here pseudo-constant symbolizes the path of currently running file
 var Here string
+// Cfg represents contents of the configuration file.
+var Cfg config
 
 func main() {
 	//gin.SetMode(gin.ReleaseMode)
 	Here, _ = os.Executable()
+	Cfg = loadConfig("config.json")
+
 	router := gin.Default()
 
 	staticPath := getDirectoryPath("static/")
 	router.Static("/static", staticPath)
 
 	InitFrontRouter(router)
-	router.Run()
+	log.Fatalln(router.Run())
 }
