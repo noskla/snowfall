fe299710aa0a642750f186580bf1d060384643b0
noskla <a@horsi.fi>
2021-02-17 03:52:49 +0100

    ⚡ Simple key authentication implementation - Key validation

--- authorization.go
+++ authorization.go
@@ -2,10 +2,10 @@ package main
 
 import "time"
 
-// generateAPIKey forges an SQL query to send to Postgres database, then
+// GenerateAPIKey forges an SQL query to send to Postgres database, then
 // returns if the process was successful with either generated key or
 // and error alongside it.
-func generateAPIKey(userID string, expiryDateUnix int64) (bool, string) {
+func GenerateAPIKey(userID string, expiryDateUnix int64) (bool, string) {
 
 	tx, err := Database.Begin()
 	if errorOccurred(err, false) {
@@ -17,7 +17,12 @@ func generateAPIKey(userID string, expiryDateUnix int64) (bool, string) {
 		return false, "Database error (Transaction prepare 1)"
 	}
 	var userName string
-	err = stmt.QueryRow(userID).Scan(&userName)
+	row := stmt.QueryRow(userID)
+	if row == nil {
+		return false, "User does not exist."
+	}
+
+	err = row.Scan(&userName)
 	if errorOccurred(err, false) {
 		return false, "Database error (Transaction query, scan 1)"
 	}
@@ -45,5 +50,36 @@ func generateAPIKey(userID string, expiryDateUnix int64) (bool, string) {
 	}
 
 	return true, authKey
+
+}
+
+// ValidateAPIKey checks the given key and returns if the query succeeded and
+// if true; the permission level assigned to related user.
+// 0 - key does not exist, 1 - admin: false, 2 - admin: true
+func ValidateAPIKey(apiKey string) (bool, uint8) {
+
+	tx, err := Database.Begin()
+	if errorOccurred(err, false) {
+		return false, 0
+	}
+
+	stmt, err := tx.Prepare(`select users.admin from keys left join users on users.id = keys.owner where keys.authkey = $1`)
+	if errorOccurred(err, false) {
+		return false, 0
+	}
+	defer stmt.Close()
+
+	var isAdmin bool
+	err = stmt.QueryRow(apiKey).Scan(&isAdmin)
+	if errorOccurred(err, false) {
+		return false, 0
+	}
+
+	permLevel := uint8(1)
+	if isAdmin {
+		permLevel++
+	}
+
+	return true, permLevel
 
 }
