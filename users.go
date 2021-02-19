d0bdb119e1849d784e1a2d8567bd77492f68a365
noskla <a@horsi.fi>
2021-02-19 02:18:03 +0100

    ⚡ User credential validation

--- users.go
+++ users.go
@@ -4,6 +4,7 @@ import (
 	"github.com/streadway/amqp"
 	"golang.org/x/crypto/bcrypt"
 	"regexp"
+	"time"
 )
 
 // createUser forges an SQL query to send to Postgres database, then
@@ -98,5 +99,86 @@ func confirmDiscord(userID string, discordConfirm string) (bool, string) {
 	}
 
 	return true, "Discord confirmed"
+
+}
+
+type userBasicInformation struct {
+	ID      string `json:"userID"`
+	isAdmin bool   `json:"isAdmin"`
+	apiKey  string `json:"apiKey"`
+}
+func validateCredentials(username string, password string, generateKey bool) (bool, string, userBasicInformation) {
+
+	tx, err := Database.Begin()
+	if errorOccurred(err, false) {
+		return false, "Database error (Transaction begin)", userBasicInformation{}
+	}
+
+	stmt, err := tx.Prepare(`select id, password, admin from users where name = $1`)
+	if errorOccurred(err, false) {
+		return false, "Database error (Transaction prepare)", userBasicInformation{}
+	}
+
+	var userID string
+	var passwordHashed string
+	var isAdmin bool
+
+	err = stmt.QueryRow(username).Scan(&userID, &passwordHashed, &isAdmin)
+	if errorOccurred(err, false) {
+		return false, "User does not exist", userBasicInformation{}
+	}
+
+	stmt.Close()
+
+	row := stmt.QueryRow(userID)
+	if errorOccurred(row.Err(), false) {
+		return false, "Database error (Transaction query 2)", userBasicInformation{}
+	}
+
+	err = tx.Commit()
+	if errorOccurred(err, false) {
+		return false, "Database error (Transaction commit)", userBasicInformation{}
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(passwordHashed), []byte(password))
+	if errorOccurred(err, false) {
+		return false, "Password incorrect", userBasicInformation{}
+	}
+
+	if generateKey {
+		ok, key := GenerateAPIKey(userID, int64(time.Hour * 48))
+		if !ok {
+			return false, key, userBasicInformation{}
+		}
+		return true, "Password correct", userBasicInformation{userID, isAdmin, key}
+	}
+
+	return true, "Password correct", userBasicInformation{userID, isAdmin, ""}
+}
+
+func makeUserAnAdmin(userID string) (bool, string) {
+
+	tx, err := Database.Begin()
+	if errorOccurred(err, false) {
+		return false, "Database error (Transaction begin)"
+	}
+
+	stmt, err := tx.Prepare(`update users set admin = true where id = $1`)
+	if errorOccurred(err, false) {
+		return false, "Database error (Transaction prepare)"
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRow(userID)
+	if errorOccurred(row.Err(), false) {
+		return false, "User does not exist"
+	}
+
+	err = tx.Commit()
+	if errorOccurred(err, false) {
+		return false, "Database error (Transaction commit)"
+	}
+
+	return true, "Ok"
 
 }
