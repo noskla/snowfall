5b903e69804cd3383e644093e55faa71ec3594df
noskla <a@horsi.fi>
2021-02-21 05:33:11 +0100

    ?? Ask for room list only once, initialize room list asynchronously

--- static/main.js
+++ static/main.js
@@ -1,21 +1,27 @@
 $(document).ready(() => {
 
-    api.getRooms().then(resRooms => {
-        if (resRooms.success)
-            resRooms.rooms.forEach(resRoom => {
-                $(`<a>${resRoom.name}</a>`)
-                    .addClass("roomLink")
-                    .attr("href", `/room/${resRoom.name}`)
-                    .on({
-                        click: ev => {
-                            $(this).toggleClass("active")
-                        }
-                    }).appendTo("#rooms");
-            });
-        else
-            $(`<p>${resRooms.answer}</p>`)
-                .addClass("error")
-                .appendTo("#rooms");
-    });
+    (async () => {
+        let rooms = await api.getRooms();
+        switch (typeof rooms) {
+            case "string":
+                $( `<p>${rooms.reason}</p>` )
+                    .addClass('error')
+                    .appendTo('#rooms');
+            case "object":
+                if (rooms == null)
+                    return $( `<p>Żaden pokój nie został utworzony</p>` )
+                        .addClass('error')
+                        .appendTo('#rooms');
+                rooms.forEach(r =>
+                    $( `<a>${r.name}</a>` )
+                        .addClass('roomLink')
+                        .attr('href', `/room/${r.name}`)
+                        .on({
+                            click: ev =>
+                                $(this).toggleClass('active')})
+                        .appendTo('#rooms')
+                );
+        }
+    })();
 
 });
